af3a0062b1f6e82dba35dbf2b73c782f8a99cf29
Admin <admin@Picos-iMac.local>
2020-08-31 18:29:45 +0700

    update Home.js for each session description

--- src/components/pages/Newsdetail.js
+++ src/components/pages/Newsdetail.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import {Helmet} from "react-helmet";
+import {FacebookShareButton, FacebookIcon} from "react-share";
 
 const NewsDetail=()=>{
     const {newsid}=useParams();
@@ -28,7 +29,7 @@ const NewsDetail=()=>{
     },[]);
 
     return(
-        <main className="main" role="main" className="about-pageXXX container">
+        <main className="main" role="main" className="container">
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>{data.title}</title>
@@ -38,9 +39,19 @@ const NewsDetail=()=>{
 
             <div className="containerXXX">
                 <div className="text-center bg-dark"><img src={data.cover} className="img-fluid" /></div>
-                <div className="mt-3"><h3>{data.title}</h3></div>
-                <p className="store-desc-pXXX py-4"><div dangerouslySetInnerHTML={{ __html: data.content }} /></p>
+                <div className="my-3"><h3>{data.title}</h3></div>
+                <p className="store-desc-pXXX py-2"><div dangerouslySetInnerHTML={{ __html: data.content }} /></p>
                 <div></div>
+            </div>
+
+            <div>
+                <FacebookShareButton 
+                    url={`http://localhost:8000/news/${newsid}`}
+                    quote={data.title}
+                    hashtag="#foreat"
+                    className="">
+                    <FacebookIcon size={36} />
+                </FacebookShareButton>
             </div>
 
         </main>
