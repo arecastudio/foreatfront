cde6d8d3375214b47a8158499f0fd0ccbbe70ad7
Admin <admin@Picos-iMac.local>
2020-09-01 15:50:30 +0700

    fronted fixed with fill dummy data

--- src/components/pages/Newsdetail.js
+++ src/components/pages/Newsdetail.js
@@ -8,10 +8,6 @@ const NewsDetail=()=>{
     const {newsid}=useParams();
     const [data,setData]=useState({});
 
-    const getSelfUrl=()=>{
-        return "result: ";
-    }
-
     useEffect(()=>{
         try {
             //const response= axios.get('http://localhost:8000/store/');
@@ -34,20 +30,20 @@ const NewsDetail=()=>{
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>{data.title}</title>
-                <link rel="canonical" href={`http://localhost:8000/news/${newsid}/`} />
+                {/*<link rel="canonical" href={`http://localhost:8000/news/${newsid}/`} />*/}
                 <meta name="description" content={{ __html: data.content }}/>
             </Helmet>
 
             <div className="containerXXX">
                 <div className="text-center bg-dark"><img src={data.cover} className="img-fluid" /></div>
                 <div className="my-3"><h3>{data.title}</h3></div>
-                <p className="store-desc-pXXX py-2"><div dangerouslySetInnerHTML={{ __html: data.content }} /></p>
+                <p className="store-desc-pXXX py-2"><span dangerouslySetInnerHTML={{ __html: data.content }} /></p>
                 <div></div>
             </div>
 
             <div>
                 <FacebookShareButton 
-                    url={`http://localhost:8000/news/${newsid}`}
+                    url=""
                     quote={data.title}
                     hashtag="#foreat"
                     className="">
